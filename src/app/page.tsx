d7d43631430b9fcb21b30d7bd5a92f70580db5f2
Sajjad Hussain <sajjaditprofessional@gmail.com>
2023-06-08 03:08:47 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Nav from "../../component/Nav"
     <h1>HOME ===  Hello MyNew App 01</h1>
     <p> HOME === Learning Next.js</p>
     <h3>Hello Pakistan </h3>
+    <h4>mynew-app01</h4>
 <Card/>
     </div>
   )
